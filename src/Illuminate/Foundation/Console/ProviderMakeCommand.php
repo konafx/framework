80c1453745373322334f004ae2513c0ef3841ca8
konafx <afxlieonon@gmail.com>
2021-10-05 11:03:08 +0900

    Add option defer on make:provider

--- src/Illuminate/Foundation/Console/ProviderMakeCommand.php
+++ src/Illuminate/Foundation/Console/ProviderMakeCommand.php
@@ -34,7 +34,15 @@ class ProviderMakeCommand extends GeneratorCommand
      */
     protected function getStub()
     {
-        return __DIR__.'/stubs/provider.stub';
+        $stub = null;
+
+        if ($this->option('defer')) {
+            $stub = '/stubs/provider.deferrable.stub';
+        }
+
+        $stub = $stub ?? '/stubs/provider.stub';
+
+        return __DIR__.$stub;
     }
 
     /**
@@ -46,5 +54,18 @@ class ProviderMakeCommand extends GeneratorCommand
     protected function getDefaultNamespace($rootNamespace)
     {
         return $rootNamespace.'\Providers';
+    }
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['force', null, InputOption::VALUE_NONE, 'Create the class even if the controller already exists'],
+            ['defer', 'd', InputOption::VALUE_NONE, 'Generate a deferrable provider class.'],
+        ];
     }
 }
